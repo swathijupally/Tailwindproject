e472c825b65aeb90a1fec5bc96621b1ed1a6d40b
swathijupally <swathi.j@acelinetech.com>
2022-10-13 15:26:08 +0530

    init

---
+++ react-js/src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Example from "./Example";
+import Items from "./Items";
+
+import Promo from "./Promo";
+
+
+
+
+
+function App() {
+
+   return (
+     
+     <div>
+      <Example/>
+    
+      <Items/>
+      <Promo/>
+        
+     </div>
+   );
+  }
+
+
+export default App;
